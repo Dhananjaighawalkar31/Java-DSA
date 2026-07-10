f37397cc277df167997ea98e90d03630ec644c89
Dhananjai Ghawalkar <dhananjaighawalkar68@gmail.com>
2026-07-10 12:51:52 +0530

    Post order traversal using recursion

--- src/Trees/Tree.java
+++ src/Trees/Tree.java
@@ -24,16 +24,38 @@ class Tree {
         root.left.right = new Node(5);
 
         root.right.left = new Node(6);
-        preOrder(root);
+//        preOrder(root);
+//        postOrder(root);
+//        inOrder(root);
     }
 
-	private static void preOrder(Node root) {
+//	private static void inOrder(Node root) {
+//		if(root == null) {
+//			return;
+//		}
+//		inOrder(root.left);
+//		System.out.println(root.data);
+//		inOrder(root.right);
+//		
+//	}
+
+	private static void postOrder(Node root) {
 		if(root == null) {
 			return;
 		}
+		postOrder(root.left);
+		postOrder(root.right);
 		System.out.println(root.data);
-		preOrder(root.left);
-		preOrder(root.right);
 		
 	}
+
+//	private static void preOrder(Node root) {
+//		if(root == null) {
+//			return;
+//		}
+//		System.out.println(root.data);
+//		preOrder(root.left);
+//		preOrder(root.right);
+//		
+//	}
 }
