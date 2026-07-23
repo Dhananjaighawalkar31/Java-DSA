4e42a556fcdabe10e82dedf306ccc57663e64fcc
Dhananjai Ghawalkar <dhananjaighawalkar68@gmail.com>
2026-07-23 11:28:33 +0530

    Vertical Traversal

--- src/Trees/Tree.java
+++ src/Trees/Tree.java
@@ -5,35 +5,50 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Stack;
+import java.util.TreeMap;
 
 class Node {
 
-    int data;
-    Node left;
-    Node right;
+	int data;
+	Node left;
+	Node right;
 
-    Node(int data){
-        this.data = data;
-       
+	Node(int data) {
+		this.data = data;
+
+	}
+}
+
+class Tuple {
+    Node node;
+    int column;
+    int row;
+
+    public Tuple(Node node, int column, int row) {
+        this.node = node;
+        this.column = column;
+        this.row = row;
     }
 }
 
 class Tree {
-    int max = 0;
-    public static void main(String[] args) {
+	int max = 0;
 
-        Node root = new Node(1);
+	public static void main(String[] args) {
 
-        root.left = new Node(2);
-        root.right = new Node(3);
+		Node root = new Node(1);
 
-        root.left.left = new Node(4);
-        root.left.right = new Node(5);
+		root.left = new Node(2);
+		root.right = new Node(3);
 
-        root.right.left = new Node(6);
-        Tree t = new Tree();
+		root.left.left = new Node(4);
+		root.left.right = new Node(5);
+
+		root.right.left = new Node(6);
+		Tree t = new Tree();
 //        preOrder(root);
 //        postOrder(root);
 //        inOrder(root);
@@ -53,294 +68,380 @@ class Tree {
 //        BinaryTreePath(root);
 //        t.maxPathSum(root);
 //        zigzagLevelOrder(root);
-        boundaryTraversalInAntiClockWise(root);
-    }
-    private static List<Integer> boundaryTraversalInAntiClockWise(Node root) {
-		if(root == null) {
+		List<List<Integer>> ans = t.verticalTraversal(root);
+
+		for (List<Integer> list : ans) {
+		    System.out.println(list);
+		}
+	}
+
+	public List<List<Integer>> verticalTraversal(Node root) {
+
+	    List<List<Integer>> ans = new ArrayList<>();
+
+	    if (root == null)
+	        return ans;
+
+	    TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
+
+	    Queue<Tuple> q = new LinkedList<>();
+
+	    // Root starts at column = 0, row = 0
+	    q.offer(new Tuple(root, 0, 0));
+
+	    while (!q.isEmpty()) {
+
+	        Tuple tuple = q.poll();
+
+	        Node node = tuple.node;
+	        int column = tuple.column;
+	        int row = tuple.row;
+
+	        // Create column if it doesn't exist
+	        if (!map.containsKey(column)) {
+	            map.put(column, new TreeMap<>());
+	        }
+
+	        // Create row inside that column if it doesn't exist
+	        if (!map.get(column).containsKey(row)) {
+	            map.get(column).put(row, new PriorityQueue<>());
+	        }
+
+	        // Store node value
+	        map.get(column).get(row).offer(node.data);
+
+	        // Left child
+	        if (node.left != null) {
+	            q.offer(new Tuple(node.left, column - 1, row + 1));
+	        }
+
+	        // Right child
+	        if (node.right != null) {
+	            q.offer(new Tuple(node.right, column + 1, row + 1));
+	        }
+	    }
+
+	    // Read values from TreeMap
+	    for (TreeMap<Integer, PriorityQueue<Integer>> rows : map.values()) {
+
+	        List<Integer> list = new ArrayList<>();
+
+	        for (PriorityQueue<Integer> pq : rows.values()) {
+
+	            while (!pq.isEmpty()) {
+	                list.add(pq.poll());
+	            }
+	        }
+
+	        ans.add(list);
+	    }
+
+	    return ans;
+	}
+
+	private static List<Integer> boundaryTraversalInAntiClockWise(Node root) {
+		if (root == null) {
 			return null;
 		}
 		List<Integer> al = new ArrayList<>();
-		al.add(root.data);
-		if(root.left != null) {
-			getLeft(root.left,al);
+		if (!isLeaf(root)) {
+			al.add(root.data);
 		}
-		getLeaf(root,al);
-		if(root.right != null) {
-			getRight(root,al);
+		if (root.left != null) {
+			getLeft(root.left, al);
+		}
+		getLeaf(root, al);
+		if (root.right != null) {
+			getRight(root.right, al);
 		}
 		return al;
-		
+
 	}
-    
+
 	private static void getRight(Node root, List<Integer> al) {
 		Stack<Integer> st = new Stack<>();
 		Node curr = root;
-		while(!isLeaf(curr)) {
-			if(curr.right != null) {
-				st.push(curr.data);
+		while (curr != null) {
+			if (curr.right != null) {
+				if (!isLeaf(curr)) {
+					st.push(curr.data);
+				}
 				curr = curr.right;
-			}else {
-				st.push(curr.data);
+			} else {
+				if (!isLeaf(curr)) {
+					st.push(curr.data);
+				}
 				curr = curr.left;
 			}
 		}
-		while(!st.isEmpty()) {
+		while (!st.isEmpty()) {
 			al.add(st.pop());
 		}
-		
+
 	}
+
 	private static void getLeaf(Node root, List<Integer> al) {
-		if(root == null) {
+		if (root == null) {
 			return;
 		}
-		getLeaf(root.left,al);
-		if(isLeaf(root)) {
+		getLeaf(root.left, al);
+		if (isLeaf(root)) {
 			al.add(root.data);
 		}
-		getLeaf(root.right,al);
-		
+		getLeaf(root.right, al);
+
 	}
+
 	private static void getLeft(Node root, List<Integer> al) {
 		Node curr = root;
-		while(!isLeaf(curr)) {
-			if(curr.left != null) {
-				al.add(curr.data);
+		while (curr != null) {
+			if (curr.left != null) {
+				if (!isLeaf(curr)) {
+					al.add(curr.data);
+				}
 				curr = curr.left;
-			}else {
-				al.add(curr.data);
+			} else {
+				if (!isLeaf(curr)) {
+					al.add(curr.data);
+				}
 				curr = curr.right;
 			}
 		}
-		
+
 	}
+
 	private static boolean isLeaf(Node root) {
-		if(root.left == null && root.right == null) {
+		if (root.left == null && root.right == null) {
 			return true;
 		}
 		return false;
 	}
+
 	private static List<List<Integer>> zigzagLevelOrder(Node root) {
-    	List<List<Integer>> res = new ArrayList<>();
-    	if(root == null) {
-    		return res;
-    	}
-    	Queue<Node> q = new ArrayDeque<>();
-    	q.offer(root);
-    	boolean b = true;
-    	while(!q.isEmpty()) {
-    		int n = q.size();
-    		List<Integer> al = new ArrayList<>(); 
-    		for(int i = 0;i<n;i++) {
-    			Node t = q.poll();
-    			if(t.left != null) {
-    				q.offer(t.left);
-    			}
-    			if(t.right != null) {
-    				q.offer(t.right);
-    			}
-    			al.add(t.data);
-    		}
-    		if(b) {
-    			res.add(al);
-    		}else {
-    			Collections.reverse(al);
-    			res.add(al);
-    		}
-    		b = !b;
-    	}
-    	return res;
-		
+		List<List<Integer>> res = new ArrayList<>();
+		if (root == null) {
+			return res;
+		}
+		Queue<Node> q = new ArrayDeque<>();
+		q.offer(root);
+		boolean b = true;
+		while (!q.isEmpty()) {
+			int n = q.size();
+			List<Integer> al = new ArrayList<>();
+			for (int i = 0; i < n; i++) {
+				Node t = q.poll();
+				if (t.left != null) {
+					q.offer(t.left);
+				}
+				if (t.right != null) {
+					q.offer(t.right);
+				}
+				al.add(t.data);
+			}
+			if (b) {
+				res.add(al);
+			} else {
+				Collections.reverse(al);
+				res.add(al);
+			}
+			b = !b;
+		}
+		return res;
+
 	}
+
 	int maxi = 0;
-    private int maxPathSum(Node root) {
+
+	private int maxPathSum(Node root) {
 		maxi = root.data;
 		helpermaxPathSum(root);
 		return maxi;
 	}
-    
+
 	private int helpermaxPathSum(Node root) {
-		if(root == null) {
+		if (root == null) {
 			return 0;
 		}
 		int lh = helpermaxPathSum(root.left);
 		int rh = helpermaxPathSum(root.right);
-		if(lh<0) {
+		if (lh < 0) {
 			lh = 0;
 		}
-		if(rh < 0) {
+		if (rh < 0) {
 			rh = 0;
 		}
-		maxi = Math.max(maxi,root.data + lh + rh);
+		maxi = Math.max(maxi, root.data + lh + rh);
 		return root.data + Math.max(lh, rh);
 	}
 
 	private static List<String> BinaryTreePath(Node root) {
-        List<String> al = new ArrayList<>();
+		List<String> al = new ArrayList<>();
 
-        if(root == null) {
-            return al;
-        }
+		if (root == null) {
+			return al;
+		}
 
-        String path = root.data + "";
-        helper(root, al, path);
-        return al;
-    }
+		String path = root.data + "";
+		helper(root, al, path);
+		return al;
+	}
 
 	private static void helper(Node root, List<String> al, String path) {
-		if(root == null) {
+		if (root == null) {
 			return;
 		}
-		if(root.left == null && root.right == null) {
+		if (root.left == null && root.right == null) {
 			al.add(path);
 			return;
 		}
-		if(root.left != null) {
-			helper(root.left,al,path +"->" + root.left.data);
+		if (root.left != null) {
+			helper(root.left, al, path + "->" + root.left.data);
 		}
-		if(root.right != null) {
-			helper(root.right,al,path +"->" + root.right.data);
+		if (root.right != null) {
+			helper(root.right, al, path + "->" + root.right.data);
 		}
-		
+
 	}
 
 	private static boolean isBalanced(Node root) {
-		if(helper(root) == -1) {
+		if (helper(root) == -1) {
 			return false;
 		}
 		return true;
-		
+
 	}
 
 	private static int helper(Node root) {
-		if(root == null) {
+		if (root == null) {
 			return 0;
 		}
 		int lh = helper(root.left);
-		if(lh == -1) {
+		if (lh == -1) {
 			return -1;
 		}
 		int rh = helper(root.right);
-		if(rh == -1) {
+		if (rh == -1) {
 			return -1;
 		}
-		int diff = Math.abs(lh-rh);
-		if(diff > 1) {
+		int diff = Math.abs(lh - rh);
+		if (diff > 1) {
 			return -1;
 		}
 		return 1 + Math.max(lh, rh);
 	}
 
-	private  int diameterOfTree(Node root) {
-		if(root == null) {
+	private int diameterOfTree(Node root) {
+		if (root == null) {
 			return 0;
 		}
 		int lh = diameterOfTree(root.left);
 		int rh = diameterOfTree(root.right);
-		max = Math.max(max,lh+rh);
-		return 1 + Math.max(lh,rh);
+		max = Math.max(max, lh + rh);
+		return 1 + Math.max(lh, rh);
 	}
 
 	private static boolean hasPathSum(Node root, int targetSum) {
-		if(root.left == null && root.right == null && targetSum - root.data == 0) {
+		if (root.left == null && root.right == null && targetSum - root.data == 0) {
 			return true;
 		}
-		if(root.left == null && root.right == null) {
+		if (root.left == null && root.right == null) {
 			return false;
 		}
-		targetSum = targetSum-root.data;
-		return hasPathSum(root.left,targetSum) || hasPathSum(root.right,targetSum);
-		
+		targetSum = targetSum - root.data;
+		return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum);
+
 	}
 
 	private static Node invertTree(Node root) {
-		
-    	if(root == null) {
-    		return null;
-    	}
-    	
-    	invertTree(root.left);
-    	invertTree(root.right);
-    	Node dummy = root.left;
-    	root.left = root.right;
-    	root.right = dummy;
-    	return root;
-		
+
+		if (root == null) {
+			return null;
+		}
+
+		invertTree(root.left);
+		invertTree(root.right);
+		Node dummy = root.left;
+		root.left = root.right;
+		root.right = dummy;
+		return root;
+
 	}
 
 	private static int countNodes(Node root) {
-    	if(root==null) {
-    		return 0;
-    	}
+		if (root == null) {
+			return 0;
+		}
 		int left = countNodes(root.left);
 		int right = countNodes(root.right);
-		return 1+ left + right;
-		
+		return 1 + left + right;
+
 	}
-    public boolean isSameTree(Node p, Node q) {
-        if(p == null && q == null){
-            return true;
-        }
-        if(p == null || q == null || p.data != q.data){
-            return false;
-        }
-        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
-    }
+
+	public boolean isSameTree(Node p, Node q) {
+		if (p == null && q == null) {
+			return true;
+		}
+		if (p == null || q == null || p.data != q.data) {
+			return false;
+		}
+		return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+	}
 
 	private static int minDepth(Node root) {
-    	if(root == null) {
-    		return 0;
-    	}
-		if(root.right == null && root.left == null) {
+		if (root == null) {
+			return 0;
+		}
+		if (root.right == null && root.left == null) {
 			return 1;
 		}
 		int l = 0;
 		int r = 0;
-		if(root.left != null) {
+		if (root.left != null) {
 			l = minDepth(root.left);
 		}
-		if(root.right != null) {
+		if (root.right != null) {
 			r = minDepth(root.right);
 		}
-		if(l>0 && r >0) {
+		if (l > 0 && r > 0) {
 			return 1 + Math.min(l, r);
 		}
-		if(l>0 && r==0) {
+		if (l > 0 && r == 0) {
 			return 1 + l;
 		}
-		return 1+r;
-		
+		return 1 + r;
+
 	}
 
 	private static int maxDepth(Node root) {
-		if(root == null) {
+		if (root == null) {
 			return 0;
 		}
 		int a = maxDepth(root.left);
 		int b = maxDepth(root.right);
-		return 1+Math.max(a, b);
-		
+		return 1 + Math.max(a, b);
+
 	}
 
 	private static List<Integer> iterativePostOrder(Node root) {
 		List<Integer> al = new ArrayList<Integer>();
-		if(root == null) {
+		if (root == null) {
 			return al;
 		}
 		Stack<Node> st1 = new Stack<>();
 		Stack<Node> st2 = new Stack<>();
 		st1.push(root);
 		Node x = root;
-		while(!st1.isEmpty()) {
+		while (!st1.isEmpty()) {
 			x = st1.pop();
 			st2.push(x);
-			if(x.left != null) {
+			if (x.left != null) {
 				st1.push(x.left);
 			}
-			if(x.right != null) {
+			if (x.right != null) {
 				st1.push(x.right);
 			}
 		}
-		while(!st2.isEmpty()) {
+		while (!st2.isEmpty()) {
 			Node k = st2.pop();
 			al.add(k.data);
 		}
@@ -351,12 +452,12 @@ class Tree {
 		List<Integer> al = new ArrayList<>();
 		Stack<Node> st = new Stack<>();
 		Node node = root;
-		while(true) {
-			if(node != null) {
+		while (true) {
+			if (node != null) {
 				st.push(node);
 				node = node.left;
-			}else {
-				if(st.isEmpty()) {
+			} else {
+				if (st.isEmpty()) {
 					break;
 				}
 				node = st.pop();
@@ -365,90 +466,90 @@ class Tree {
 			}
 		}
 		return al;
-		
+
 	}
 
 	private static void iterativePreOrder(Node root) {
 		Stack<Node> st = new Stack<>();
 		st.push(root);
-		while(!st.isEmpty()) {
+		while (!st.isEmpty()) {
 			Node curr = st.pop();
 			System.out.print(curr.data);
-			if(curr.right != null) {
+			if (curr.right != null) {
 				st.push(curr.right);
 			}
-			if(curr.left != null) {
+			if (curr.left != null) {
 				st.push(curr.left);
 			}
 		}
-		
+
 	}
 
 	private static List<List<Integer>> levelWiseTraversal(Node root) {
 
-        List<List<Integer>> res = new ArrayList<>();
+		List<List<Integer>> res = new ArrayList<>();
 
-        if (root == null) {
-            return res;
-        }
+		if (root == null) {
+			return res;
+		}
 
-        Queue<Node> q = new LinkedList<>();
-        q.offer(root);
+		Queue<Node> q = new LinkedList<>();
+		q.offer(root);
 
-        while (!q.isEmpty()) {
+		while (!q.isEmpty()) {
 
-            int levelSize = q.size();
+			int levelSize = q.size();
 
-            List<Integer> level = new ArrayList<>();
+			List<Integer> level = new ArrayList<>();
 
-            for (int i = 0; i < levelSize; i++) {
+			for (int i = 0; i < levelSize; i++) {
 
-                Node node = q.poll();
+				Node node = q.poll();
 
-                level.add(node.data);
+				level.add(node.data);
 
-                if (node.left != null) {
-                    q.offer(node.left);
-                }
+				if (node.left != null) {
+					q.offer(node.left);
+				}
 
-                if (node.right != null) {
-                    q.offer(node.right);
-                }
-            }
+				if (node.right != null) {
+					q.offer(node.right);
+				}
+			}
 
-            res.add(level);
-        }
+			res.add(level);
+		}
 
-        return res;
-    }
+		return res;
+	}
 
 	private static void inOrder(Node root) {
-		if(root == null) {
+		if (root == null) {
 			return;
 		}
 		inOrder(root.left);
 		System.out.println(root.data);
 		inOrder(root.right);
-		
+
 	}
 
 	private static void postOrder(Node root) {
-		if(root == null) {
+		if (root == null) {
 			return;
 		}
 		postOrder(root.left);
 		postOrder(root.right);
 		System.out.println(root.data);
-		
+
 	}
 
 	private static void preOrder(Node root) {
-		if(root == null) {
+		if (root == null) {
 			return;
 		}
 		System.out.println(root.data);
 		preOrder(root.left);
 		preOrder(root.right);
-		
+
 	}
 }
